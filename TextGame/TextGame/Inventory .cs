b739f44b74ed94de966e5a113ef30025425da986
toadsam <toadsam@naver.com>
2023-08-21 11:51:50 +0900

    [ADD]기본과제까지 구현완료(-재훈)
    기본과제까지는 구현완료 상점까지도 구현 할려고 함.

---
+++ TextGame/TextGame/Inventory .cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextGame
+{
+    public class Inventory
+    {
+        public string ItemName { get; }
+        public string WhatAbility { get; }
+
+        public int AbilityNunber { get; }
+
+        public string Explanation { get; }
+
+        public bool IsWearing { get; set; }
+
+        public int Gold { get; set; }
+        public Inventory(string itemname, string whatabilit, int abilitynumber, string explantion, bool iswearing)
+        {
+            ItemName = itemname;
+            WhatAbility = whatabilit;
+            AbilityNunber = abilitynumber;
+            Explanation = explantion;
+            IsWearing = iswearing;
+
+
+        }
+
+        public Inventory(string itemname, string whatabilit, int abilitynumber, string explantion, bool iswearing,int gold)
+        {
+            ItemName = itemname;
+            WhatAbility = whatabilit;
+            AbilityNunber = abilitynumber;
+            Explanation = explantion;
+            IsWearing = iswearing;
+            Gold = gold;
+
+        }
+    }
+}
--- TextGame/TextGame/Program.cs
+++ TextGame/TextGame/Program.cs
@@ -7,6 +7,14 @@ namespace TextGame
     internal class Program
     {
         private static Character player;
+        private static Inventory ironarmor;
+        private static Inventory oldsword;
+        private static Inventory trainingarmor;
+        private static Inventory spartanarmor;
+        private static Inventory bronzeaxe;
+        private static Inventory spartaspear;
+        static List<Inventory> Item = new List<Inventory>(); 
+        static List<Inventory> StoreItem = new List<Inventory>();
         static void Main(string[] args)
         {
             GameDataSetting();
@@ -14,14 +22,26 @@ namespace TextGame
             Console.WriteLine("Hello, World!");
         }
 
-        static void GameDataSetting()
+         static void GameDataSetting()
         {
             // 캐릭터 정보 세팅
              player = new Character("Chad", "전사", 1, 10, 5, 100, 1500);
+            ironarmor = new Inventory("무쇠갑옷", "방어력 + ", 5, "무쇠로 만들어져 튼튼한 갑옷입니다.",false);
+            trainingarmor = new Inventory("수련자 갑옷", "방어력 + ", 5, "수련에 도움을 주는 갑옷입니다.", false,1000);
+            spartanarmor = new Inventory("무쇠갑옷", "방어력 + ", 15, "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", false,3500);
+            oldsword = new Inventory("낡은 검", "공격력 + ", 2, "쉽게 볼 수 있는 낡은 검 입니다.", false);
+            bronzeaxe = new Inventory("청동 도끼", "공격력 + ", 5, "어디선가 사용됐던거 같은 도끼입니다.", false,1500);
+            spartaspear = new Inventory("스파르타의 창", "공격력 + ", 7, "스파르타의 전사들이 사용했다는 전설의 창입니다.", false,2000);
+            Item.Add(ironarmor);
+            Item.Add(oldsword);
+            StoreItem.Add(trainingarmor);
+            StoreItem.Add(spartanarmor);
+            StoreItem.Add(bronzeaxe);
+            StoreItem.Add(spartaspear);
 
             // 아이템 정보 세팅
         }
-
+        
         static void DisplayGameIntro()
         {
             Console.Clear();
@@ -31,10 +51,11 @@ namespace TextGame
             Console.WriteLine();
             Console.WriteLine("1. 상태보기");
             Console.WriteLine("2. 인벤토리");
+            Console.WriteLine("3. 상점");
             Console.WriteLine();
             Console.WriteLine("원하시는 행동을 입력해주세요.");
 
-            int input = CheckValidInput(1, 2);
+            int input = CheckValidInput(1, 3);
             switch (input)
             {
                 case 1:
@@ -42,23 +63,196 @@ namespace TextGame
                     break;
 
                 case 2:
-                    // 작업해보기
+                    DIsplayInventory();
+                    break;
+                case 3:
+                    DisplayStore();
+                    break;
+                    
+            }
+
+        }
+
+        static void DisplayStore()
+        {
+            Console.Clear();
+            Console.WriteLine("상점");
+            Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
+            Console.WriteLine("");
+
+            Console.WriteLine("[보유 골드]");
+            Console.WriteLine($"{player.Gold} G");
+            Console.WriteLine("");
+
+            Console.WriteLine("[아이템 목록]");
+            for (int i = 0; i < StoreItem.Count; i++)
+            {
+                Console.WriteLine($"-{i + 1} {StoreItem[i].ItemName}    |  {StoreItem[i].WhatAbility}{StoreItem[i].AbilityNunber} | {StoreItem[i].Explanation}   |  {StoreItem[i].Gold}G");
+            }
+            Console.WriteLine("1. 아이템 구매");
+            Console.WriteLine("0. 나가기");
+
+            int input = CheckValidInput(0, 0);
+            switch (input)
+            {
+                case 0:
+                    DisplayGameIntro();
+                    break;
+
+                case 1:
+
                     break;
             }
 
         }
 
-       static void DisplayMyInfo()
+        static void DIsplayStoreSsll()
+        {
+            Console.Clear();
+            Console.WriteLine("상점 - 아이템 구매");
+            Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
+            Console.WriteLine("");
+
+            Console.WriteLine("[보유 골드]");
+            Console.WriteLine($"{player.Gold} G");
+            Console.WriteLine("");
+
+            Console.WriteLine("[아이템 목록]");
+            for (int i = 0; i < StoreItem.Count; i++)
+            {
+                Console.WriteLine($"-{i + 1} {StoreItem[i].ItemName}    |  {StoreItem[i].WhatAbility}{StoreItem[i].AbilityNunber} | {StoreItem[i].Explanation}   |  {StoreItem[i].Gold}G");
+            }
+            Console.WriteLine("1. 아이템 구매");
+            Console.WriteLine("0. 나가기");
+        }
+        static void ItemListPrint()
+        {
+            for (int i = 0; i < Item.Count; i++)
+            {
+               
+                if (Item[i].IsWearing == true)
+                {
+                    Console.WriteLine($"- [E]{Item[i].ItemName}    |  {Item[i].WhatAbility}{Item[i].AbilityNunber} | {Item[i].Explanation}");
+                }
+                else
+                {
+                    Console.WriteLine($"-{Item[i].ItemName}    |  {Item[i].WhatAbility}{Item[i].AbilityNunber} | {Item[i].Explanation}");
+                }
+            } 
+        }
+        static void ItemManagementPrint(List<Inventory>item)
+        {
+            for (int i = 0; i < Item.Count; i++)
+            {
+
+                if (Item[i].IsWearing == true)
+                {
+                    Console.WriteLine($"- {i + 1} [E]{Item[i].ItemName}    |  {Item[i].WhatAbility}{Item[i].AbilityNunber} | {Item[i].Explanation}");                  
+                }
+                else
+                {
+                    Console.WriteLine($"-{i + 1} {Item[i].ItemName}    |  {Item[i].WhatAbility}{Item[i].AbilityNunber} | {Item[i].Explanation}");                
+                }
+            }
+        }
+       
+        static void DisplayItemManagement()
+        {
+            Console.Clear();
+
+            Console.WriteLine("인벤토리 - 장착 관리");
+            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("[아이템 목록]");
+            ItemManagementPrint(Item);
+
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+
+
+            while (true)
+            {
+                //SelectItem();
+               
+                if(SelectItem() == false)
+                {
+                    break;
+                }
+                Console.Clear();           
+                Console.WriteLine("인벤토리 - 장착 관리");
+                Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+                Console.WriteLine();
+                Console.WriteLine("[아이템 목록]");
+                ItemManagementPrint(Item);
+
+                Console.WriteLine();
+                Console.WriteLine("0. 나가기");
+            }
+            DisplayGameIntro();
+        }
+
+        static bool SelectItem()
+        {
+
+            
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("i :" + i);
+            if (i-1>=0 && i-1 < Item.Count)
+            {
+                if (Item[i-1].IsWearing == true)
+                {                          
+                    Item[i - 1].IsWearing = false;
+                }
+                else
+                {                   
+                    Item[i - 1].IsWearing = true;
+                }
+                return true;
+            }
+            else if (i == 0)
+            {
+                
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("잘못된 입력입니다.");
+                return true;
+            }
+           
+        }
+        static void AddAbility(string AbilityName)
+        {
+            for (int i = 0; i < Item.Count; i++)
+            {
+                if (Item[i].IsWearing == true)
+                {
+                    if (Item[i].WhatAbility == AbilityName)
+                    {
+                        Console.Write($"(+{Item[i].AbilityNunber})");
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+        static void DisplayMyInfo()
         {
             Console.Clear();
 
             Console.WriteLine("상태보기");
             Console.WriteLine("캐릭터의 정보르 표시합니다.");
             Console.WriteLine();
-            Console.WriteLine($"Lv.{player.Level}");
             Console.WriteLine($"{player.Name}({player.Job})");
-            Console.WriteLine($"공격력 :{player.Atk}");
-            Console.WriteLine($"방어력 : {player.Def}");
+            Console.WriteLine($"Lv.{player.Level}");
+            
+                
+                    
+            Console.Write($"공격력 :{player.Atk}");
+            AddAbility("공격력 + ");
+            Console.Write($"방어력 : {player.Def}");
+            AddAbility("방어력 + ");
+
+
             Console.WriteLine($"체력 : {player.Hp}");
             Console.WriteLine($"Gold : {player.Gold} G");
             Console.WriteLine();
@@ -70,7 +264,44 @@ namespace TextGame
                 case 0:
                     DisplayGameIntro();
                     break;
+
+                case 1:
+                   
+                    break;
             }
+        }
+        
+        static void DIsplayInventory() 
+        {
+            Console.Clear();
+
+            Console.WriteLine("인벤토리");
+            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("[아이템 목록]");
+            ItemListPrint();
+
+            Console.WriteLine();
+            Console.WriteLine("1. 장착관리");
+            Console.WriteLine("0. 나가기");
+
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+
+            int input = CheckValidInput(0, 1);
+            switch (input)
+            {
+                case 0:
+                    DisplayGameIntro();
+                    break;
+                case 1:
+                    DisplayItemManagement();
+                    break;
+
+
+            }
+
+          
         }
 
         static int CheckValidInput(int min, int max)  //체크하는 함수
@@ -91,25 +322,7 @@ namespace TextGame
         }
     }
     
-    public class Character
-    {
-        public string Name { get; }
-        public string Job { get; }
-        public int Level { get; }
-        public int Atk { get; }
-        public int Def { get; }
-        public int Hp { get; }
-        public int Gold { get; }
+   
 
-        public Character(string name, string job, int level, int atk, int def, int hp, int gold)
-        {
-            Name = name;
-            Job = job;
-            Level = level;
-            Atk = atk;
-            Def = def;
-            Hp = hp;
-            Gold = gold;
-        }
-    }
+    
 }
