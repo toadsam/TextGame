77add449d938fb0a0d90683261d2ede6f48acfa4
toadsam <toadsam@naver.com>
2023-08-23 12:36:50 +0900

    [ADD] 던전구현 완료 (-재훈)
    던전 난이도 별로 구현

--- TextGame/TextGame/Program.cs
+++ TextGame/TextGame/Program.cs
@@ -1,10 +1,4 @@
-﻿using System.Numerics;
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Xml.Linq;
-using System.Xml.Serialization;
-
-namespace TextGame
+﻿namespace TextGame
 {
     internal class Program
     {
@@ -17,7 +11,7 @@ namespace TextGame
         private static Inventory spartaspear;
         private static Inventory meat;
         private static Inventory computer;
-        static List<Inventory> Item = new List<Inventory>(); 
+        static List<Inventory> Item = new List<Inventory>();
         static List<Inventory> StoreItem = new List<Inventory>();
         static Random random;
         static bool isWin = true;
@@ -34,18 +28,18 @@ namespace TextGame
             Console.WriteLine("Hello, World!");
         }
 
-         static void GameDataSetting()
+        static void GameDataSetting()
         {
             // 캐릭터 정보 세팅
-             player = new Character("재훈재훈", "전사", 1, 10, 5, 100, 1500);
-            ironarmor = new Inventory("무쇠갑옷", "방어력 + ", 5, "무쇠로 만들어져 튼튼한 갑옷입니다.",false);
-            trainingarmor = new Inventory("수련자 갑옷", "방어력 + ", 5, "수련에 도움을 주는 갑옷입니다.", false,1000,false);
-            spartanarmor = new Inventory("무쇠갑옷", "방어력 + ", 15, "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", false,3500,false);
+            player = new Character("재훈재훈", "전사", 1, 10, 5, 100, 1500);
+            ironarmor = new Inventory("무쇠갑옷", "방어력 + ", 5, "무쇠로 만들어져 튼튼한 갑옷입니다.", false);
+            trainingarmor = new Inventory("수련자 갑옷", "방어력 + ", 5, "수련에 도움을 주는 갑옷입니다.", false, 1000, false);
+            spartanarmor = new Inventory("무쇠갑옷", "방어력 + ", 15, "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", false, 3500, false);
             oldsword = new Inventory("낡은 검", "공격력 + ", 2, "쉽게 볼 수 있는 낡은 검 입니다.", false);
-            bronzeaxe = new Inventory("청동 도끼", "공격력 + ", 5, "어디선가 사용됐던거 같은 도끼입니다.", false,1500,false);
-            spartaspear = new Inventory("스파르타의 창", "공격력 + ", 7, "스파르타의 전사들이 사용했다는 전설의 창입니다.", false,2000,false);
+            bronzeaxe = new Inventory("청동 도끼", "공격력 + ", 5, "어디선가 사용됐던거 같은 도끼입니다.", false, 1500, false);
+            spartaspear = new Inventory("스파르타의 창", "공격력 + ", 7, "스파르타의 전사들이 사용했다는 전설의 창입니다.", false, 2000, false);
             meat = new Inventory("고기꼬기고기꼬기", "체력 + ", 2000, "고기다. 두고두고 먹을 수 있다. 엄청나다", false, 2, false);
-            computer = new Inventory("컴퓨터","방어력 + ",9999,"개발자의 필수품! 필살기다",false,2,false);
+            computer = new Inventory("컴퓨터", "방어력 + ", 9999, "개발자의 필수품! 필살기다", false, 2, false);
             Item.Add(ironarmor);
             Item.Add(oldsword);
             StoreItem.Add(trainingarmor);
@@ -55,13 +49,13 @@ namespace TextGame
             StoreItem.Add(meat);
             StoreItem.Add(computer);
             // 아이템 정보 세팅
-             totalAtk = TotalAbility("공격력 + ", player.Atk);
-             totalDef = TotalAbility("방어력 + ", player.Def);
-             totoalHP = TotalAbility("체력 + ", player.Hp);         
-             totalGold = player.Gold;
-             random = new Random();
+            totalAtk = TotalAbility("공격력 + ", player.Atk);
+            totalDef = TotalAbility("방어력 + ", player.Def);
+            totoalHP = TotalAbility("체력 + ", player.Hp);
+            totalGold = player.Gold;
+            random = new Random();
         }
-        
+
         static void DisplayGameIntro()       //게임초기화면 표시
         {
             Console.Clear();
@@ -151,22 +145,22 @@ namespace TextGame
                     break;
 
                 case 1:
-                    DungeonLevel(4,1000);  //던전권장 방어력과 얻는보상 설정
-                    DisplayDungeonResult("쉬운",isWin);
+                    DungeonLevel(4, 1000);  //던전권장 방어력과 얻는보상 설정
+                    DisplayDungeonResult("쉬운", isWin);
                     break;
 
                 case 2:
-                    DungeonLevel(11,1700);
-                    DisplayDungeonResult("일빈",isWin);
+                    DungeonLevel(11, 1700);
+                    DisplayDungeonResult("일빈", isWin);
                     break;
 
                 case 3:
-                    DungeonLevel(17,2500);
+                    DungeonLevel(17, 2500);
                     DisplayDungeonResult("어려움", isWin);
                     break;
 
                 case 4:
-                    DungeonLevel(999,9999);
+                    DungeonLevel(999, 9999);
                     DisplayDungeonResult("??", isWin);
                     break;
 
@@ -174,15 +168,15 @@ namespace TextGame
 
         }
 
-        static bool DungeonLevel(int recorecommendDefense,int gainGold)  //던전방어력과 받는돈을 받아서 던전의 성공여부의 따라 bool값을 반환하는 메서드
-        {        
+        static bool DungeonLevel(int recorecommendDefense, int gainGold)  //던전방어력과 받는돈을 받아서 던전의 성공여부의 따라 bool값을 반환하는 메서드
+        {
             preTotalHP = totoalHP;  //던전을 하기전의 총hp를 저장
             preTotalGold = totalGold; //던전을 하기전의 총 gold를 저장
-            if(totalDef >= recorecommendDefense)
+            if (totalDef >= recorecommendDefense)
             {
                 isWin = true;  //던전 클리어
                 int totalMinus = totalDef - recorecommendDefense;  //권장 방어력보다 같거나 크면
-                if (ItemHPMinus(random.Next(20,35)-totalMinus) == false) ; //체력관련 아이템이 없다면
+                if (ItemHPMinus(random.Next(20, 35) - totalMinus) == false) ; //체력관련 아이템이 없다면
                 {
                     player.Hp -= (random.Next(20, 35) - totalMinus);   //플레이어의 hp에서 체력빼기
                 }
@@ -190,10 +184,11 @@ namespace TextGame
                 totalGold += gainGold + (int)(gainGold * random.Next(totalDef, 2 * totalDef) * 0.01);  //gold보상 받기
                 player.Gold = totalGold;
             }
-               
-          else if(totalDef<recorecommendDefense) {    //권장 방어력보다 작으면
-                    if (4 >= random.Next(1, 10))       //40프로의 확률로 4보다 같거나 작다면
-                    {
+
+            else if (totalDef < recorecommendDefense)
+            {    //권장 방어력보다 작으면
+                if (4 >= random.Next(1, 10))       //40프로의 확률로 4보다 같거나 작다면
+                {
                     isWin = true;  //던전 클리어
                     int totalMinus = totalDef - recorecommendDefense;  //권장 방어력보다 같거나 크면
                     if (ItemHPMinus(random.Next(20, 35) - totalMinus) == false) ; //체력관련 아이템이 없다면
@@ -204,19 +199,22 @@ namespace TextGame
                     totalGold += gainGold + (int)(gainGold * random.Next(totalDef, 2 * totalDef) * 0.01);  //gold보상 받기
                     player.Gold = totalGold;
                 }
-                    else {  //이건 고민좀해보자                                                           
-                           totoalHP /= 2;  
-                           player.Hp = totoalHP;
-                           player.Gold = totalGold;
-                           isWin = false; //던전 실패
-                    
-                           }
+                else
+                {                                                             
+                    if (ItemHPMinus(random.Next(20, 35)) == false) ; //체력관련 아이템이 없다면
+                    {
+                        player.Hp -= (random.Next(20, 35));   //플레이어의 hp에서 체력빼기
+                    }
+                    totoalHP = TotalAbility("체력 + ", player.Hp);
+                    isWin = false; //던전 실패
+
                 }
+            }
             return isWin;
         }
         static bool ItemHPMinus(int hpMinus)  //체력관련 아이템을 장착했다면, 아이템의 체력을 깍기
         {
-            bool isMinus = true;  
+            bool isMinus = true;
             for (int i = 0; i < Item.Count; i++)
             {
                 if (Item[i].IsWearing == true)  //장착한 장비가 있다면
@@ -230,13 +228,13 @@ namespace TextGame
                     else
                     {
                         isMinus = false;                    //체력아이템이 없다면 false반환
-                        
+
                     }
                 }
             }
             return isMinus;
         }
-        static void DisplayDungeonResult (string level, bool isWin)    //던전
+        static void DisplayDungeonResult(string level, bool isWin)    //던전
         {
             Console.Clear();
             if (isWin == true)
@@ -245,7 +243,7 @@ namespace TextGame
                 Console.WriteLine("축하합니다!!");
                 Console.WriteLine($"{level} 던전을 클리어 하였습니다");
             }
-            else if(isWin == false)
+            else if (isWin == false)
             {
                 Console.WriteLine("던전 실패...!!!!!");
                 Console.WriteLine("축하합니다!!");
@@ -273,8 +271,8 @@ namespace TextGame
                     break;
             }
 
-            }
-        
+        }
+
 
 
         static void DIsplayStoreSsll()  //상점구매화면 표시
@@ -291,11 +289,11 @@ namespace TextGame
             Console.WriteLine("[아이템 목록]");
             StoreItemListPrint();
             Console.WriteLine("");
-          
+
             Console.WriteLine("0. 나가기");
             while (true)
             {
-                if(SelectSell() == false)
+                if (SelectSell() == false)
                 {
                     break;
                 }
@@ -344,10 +342,10 @@ namespace TextGame
                     Console.WriteLine($"{player.Gold}G");
                     Console.WriteLine("판매 완료!");
                     Thread.Sleep(1000);
-                } 
+                }
                 else  //플레이어가 물건을 살 때
-                {              
-                  
+                {
+
                     if (player.Gold >= StoreItem[i - 1].Gold)  //플레이어의 돈이 살 물건의 돈보다 같거나 많다면
                     {
                         StoreItem[i - 1].IsSell = true;   //물건사기
@@ -359,7 +357,7 @@ namespace TextGame
                     }
                     else
                     {
-                        Console.WriteLine("금액이 부족합니다.");  
+                        Console.WriteLine("금액이 부족합니다.");
                         Thread.Sleep(1000);
                     }
                 }
@@ -380,17 +378,17 @@ namespace TextGame
         {
             for (int i = 0; i < Item.Count; i++)
             {
-               
+
                 if (Item[i].IsWearing == true)  //플레이어가 장착중인 아이템이라면
                 {
                     Console.WriteLine($"- [E]{Item[i].ItemName}    |  {Item[i].WhatAbility}{Item[i].AbilityNunber} | {Item[i].Explanation}");
-                    
+
                 }
                 else
                 {
                     Console.WriteLine($"-{Item[i].ItemName}    |  {Item[i].WhatAbility}{Item[i].AbilityNunber} | {Item[i].Explanation}");
                 }
-            } 
+            }
         }
         static void ItemManagementPrint()  //창착관리 출력 메서드 -> ItemListPrint()에 번호만 붙임
         {
@@ -399,15 +397,15 @@ namespace TextGame
 
                 if (Item[i].IsWearing == true)
                 {
-                    Console.WriteLine($"- {i + 1} [E]{Item[i].ItemName}    |  {Item[i].WhatAbility}{Item[i].AbilityNunber} | {Item[i].Explanation}");                  
+                    Console.WriteLine($"- {i + 1} [E]{Item[i].ItemName}    |  {Item[i].WhatAbility}{Item[i].AbilityNunber} | {Item[i].Explanation}");
                 }
                 else
                 {
-                    Console.WriteLine($"-{i + 1} {Item[i].ItemName}    |  {Item[i].WhatAbility}{Item[i].AbilityNunber} | {Item[i].Explanation}");                
+                    Console.WriteLine($"-{i + 1} {Item[i].ItemName}    |  {Item[i].WhatAbility}{Item[i].AbilityNunber} | {Item[i].Explanation}");
                 }
             }
         }
-       
+
         static void DisplayItemManagement()  //아이템관리 화면
         {
             Console.Clear();
@@ -423,13 +421,13 @@ namespace TextGame
 
 
             while (true)
-            {          
-               
-                if(SelectItem() == false)
+            {
+
+                if (SelectItem() == false)
                 {
                     break;
                 }
-                Console.Clear();           
+                Console.Clear();
                 Console.WriteLine("인벤토리 - 장착 관리");
                 Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
                 Console.WriteLine();
@@ -445,13 +443,13 @@ namespace TextGame
         static bool SelectItem()  //장착선택 메서드
         {
 
-            
+
             int i = Convert.ToInt32(Console.ReadLine());  //입력받기
             Console.WriteLine("i :" + i);
-            if (i-1>=0 && i-1 < Item.Count)  //입력받은 수-1이 아이템인덱스에 있다면
+            if (i - 1 >= 0 && i - 1 < Item.Count)  //입력받은 수-1이 아이템인덱스에 있다면
             {
-                if (Item[i-1].IsWearing == true)  //이미 장착중이라면
-                {                          
+                if (Item[i - 1].IsWearing == true)  //이미 장착중이라면
+                {
                     Item[i - 1].IsWearing = false; //장착 해제
                     totalAtk = TotalAbility("공격력 + ", player.Atk);
                     totalDef = TotalAbility("방어력 + ", player.Def);
@@ -459,7 +457,7 @@ namespace TextGame
                     totalGold = player.Gold;
                 }
                 else //장착x였다면
-                {                   
+                {
                     Item[i - 1].IsWearing = true;  //장착
                     totalAtk = TotalAbility("공격력 + ", player.Atk);
                     totalDef = TotalAbility("방어력 + ", player.Def);
@@ -470,7 +468,7 @@ namespace TextGame
             }
             else if (i == 0)  //0이면
             {
-                
+
                 return false;  //false반환 -> while문을 빠져나가기 위한 것
             }
             else
@@ -478,23 +476,23 @@ namespace TextGame
                 Console.WriteLine("잘못된 입력입니다.");
                 return true;
             }
-           
+
         }
         static void AddAbility(string AbilityName)  //상태창에 장착한 아이템의 능력을 추가하는 메서드
         {
-            //int totalAbility = 0;
+            
             for (int i = 0; i < Item.Count; i++)
             {
                 if (Item[i].IsWearing == true)  //장착한 옷이라면
                 {
                     if (Item[i].WhatAbility == AbilityName)  //아이템의 능력과 입력받은 능력의 이름이 같으면
-                    {                     
+                    {
                         Console.Write($"(+{Item[i].AbilityNunber})"); //출력
                         //totalAbility += Item[i].AbilityNunber;
                     }
                 }
             }
-          //  Console.WriteLine();
+           
         }
         static int TotalAbility(string ItemAbilityName, int PlayerAbiliy)  //파라미터 값으로 함수의 이름을 넣으면 그 값이 출력됨
         {
@@ -504,7 +502,7 @@ namespace TextGame
                 if (Item[i].IsWearing == true)  //장착한 옷이라면
                 {
                     if (Item[i].WhatAbility == ItemAbilityName)  //아이템의 능력과 입력받은 능력의 이름이 같으면
-                    {                   
+                    {
                         totalAbility += Item[i].AbilityNunber;
                     }
                 }
@@ -521,9 +519,9 @@ namespace TextGame
             Console.WriteLine();
             Console.WriteLine($"{player.Name}({player.Job})");
             Console.WriteLine($"Lv.{player.Level}");
-            
-                
-                    
+
+
+
             Console.Write($"공격력 :{player.Atk}");
             AddAbility("공격력 + "); Console.Write($"  총 : {totalAtk}");
             Console.WriteLine();
@@ -549,8 +547,8 @@ namespace TextGame
 
             }
         }
-        
-        static void DIsplayInventory() 
+
+        static void DIsplayInventory()
         {
             Console.Clear();
 
@@ -580,7 +578,7 @@ namespace TextGame
 
             }
 
-          
+
         }
 
         static int CheckValidInput(int min, int max)  //체크하는 함수
@@ -600,8 +598,8 @@ namespace TextGame
             }
         }
     }
-    
-   
 
-    
+
+
+
 }
